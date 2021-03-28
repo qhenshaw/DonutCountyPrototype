04f32d9b15d4011bc2865d8abab193b581b64136
Quin Henshaw <qhenshaw@gmail.com>
2021-03-28 13:17:43 -0700

    hole is functional

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    [SerializeField] private float _lerpSpeed = 5f;
+
+    private Vector3 _position;
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        if(groundPlane.Raycast(ray, out float distance))
+        {
+            _position = ray.GetPoint(distance);
+        }
+
+        float dist = Vector3.Distance(transform.position, _position);
+        transform.position = Vector3.MoveTowards(transform.position, _position, _lerpSpeed * Time.deltaTime * dist);
+    }
+}
