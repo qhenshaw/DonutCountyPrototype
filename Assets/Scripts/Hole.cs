19f2b43fbab7bb18f072c20abb1c5a7eff3aa004
Quin Henshaw <qhenshaw@gmail.com>
2021-03-28 14:53:33 -0700

    - hole now lerps scale changes - fixed hole destroying itself - hole is now confined near origin

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -5,12 +5,20 @@ using UnityEngine.Events;
 
 public class Hole : MonoBehaviour
 {
-    [SerializeField] private float _lerpSpeed = 5f;
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _scaleSpeed = 5f;
+    [SerializeField] private float _maxRange = 30f;
 
     private Vector3 _position;
     private float _totalScore;
+    private Vector3 _targetScale;
 
     public UnityEvent<float> ScoreUpdated;
+
+    private void Start()
+    {
+        _targetScale = transform.localScale;
+    }
 
     private void Update()
     {
@@ -21,8 +29,11 @@ public class Hole : MonoBehaviour
             _position = ray.GetPoint(distance);
         }
 
+        _position = Vector3.ClampMagnitude(_position, _maxRange);
         float dist = Vector3.Distance(transform.position, _position);
-        transform.position = Vector3.MoveTowards(transform.position, _position, _lerpSpeed * Time.deltaTime * dist);
+        transform.position = Vector3.MoveTowards(transform.position, _position, _moveSpeed * Time.deltaTime * dist);
+
+        transform.localScale = Vector3.Lerp(transform.localScale, _targetScale, Time.deltaTime * _scaleSpeed);
     }
 
     public void ObjectConsumed(GameObject consumed)
@@ -30,11 +41,11 @@ public class Hole : MonoBehaviour
         if(consumed.TryGetComponent(out GrowthScore growthScore))
         {
             _totalScore += growthScore.Score;
-            transform.localScale += Vector3.one * growthScore.Score;
+            _targetScale += Vector3.one * growthScore.Score;
 
             ScoreUpdated.Invoke(_totalScore);
+
+            Destroy(consumed);
         }
-        
-        Destroy(consumed);
     }
 }
